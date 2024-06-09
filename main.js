f63c5e9201e2a553f3acf379a43f4485d719f7b3
ali hasan <alihassanraja06@gmail.com>
2024-06-10 01:46:09 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+const card = document.querySelector(".cardsection");
+
+let apiData;
+function renderProduct() {
+  axios("https://fakestoreapi.com/products")
+    .then((response) => {
+      // console.log(response.data);
+      apiData = response.data.map((item, index) => {
+        card.innerHTML += `
+            <div class="card p-2" style="width: 18rem; ">
+            <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Category : ${item.category}</h5>
+            <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
+            <p class="card-text">Price : $${item.price}</p>
+            <a id='addBtn' class="btn btn-primary" onclick='addToCard(${index})'>Add To Card</a>
+            </div>
+            </div>`;
+        return item;
+      });
+      // console.log(renderData);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+renderProduct();
+
+function filteredItem(btn) {
+  card.innerHTML = "";
+  let data = apiData.filter((item) => item.category === btn.innerHTML);
+  data.map((item, index) => {
+    card.innerHTML += `
+        <div class="card p-2" style="width: 18rem; ">
+        <img width='300' height='300' src="${item.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">Category : ${item.category}</h5>
+        <h5 class="card-title">Rating : ${item.rating.rate} Count : ${item.rating.count}</h5>
+        <p class="card-text">Price : $${item.price}</p>
+        <a id='addBtn' class="btn btn-primary" onclick='addToCard(${index})'>Add To Card</a>
+        </div>
+        </div>`;
+  });
+  // console.log(data);/
+}
+
+function allItems() {
+  renderProduct();
+  card.innerHTML = "";
+}
